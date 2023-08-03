af35920deefe7bf73c18ae556ac3c46c400430b4
“Jessica <jmychalm13@gmail.com>
2023-08-03 18:24:00 -0500

    added join table model - category_todo

--- db/schema.rb
+++ db/schema.rb
@@ -1,8 +1,15 @@
-ActiveRecord::Schema[7.0].define(version: 2023_08_03_021853) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_232254) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_todos", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "todo_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
