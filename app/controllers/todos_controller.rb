f409fb3cfcd08aa0f1e1559810d7b851b9b04c5b
“Jessica <jmychalm13@gmail.com>
2023-08-02 20:33:45 -0500

    finished todo actions and views

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,7 +5,41 @@ class TodosController < ApplicationController
   end
 
   def show
-    @todo = Todo.find(params[:id])
+    @todo = Todo.find_by(id: params[:id])
     render :show
+  end
+
+  def new
+    @todo = Todo.new
+    render :new
+  end
+
+  def create
+    todo = Todo.create(
+      user_id: params[:todo][:user_id],
+      title: params[:todo][:title],
+      deadline: params[:todo][:deadline],
+      description: params[:todo][:description],
+      completed: params[:todo][:completed],
+    )
+
+    redirect_to "/todos"
+  end
+
+  def edit
+    @todo = todo.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @todo = Todo.update(
+      user_id: params[:todo][:user_id],
+      title: params[:todo][:title],
+      deadline: params[:todo][:deadline],
+      description: params[:todo][:description],
+      completed: params[:todo][:completed],
+    )
+
+    redirect_to "/todos"
   end
 end
