0e9fa97cbe4f1939598e3cfe17c10c949c6ade84
“Jessica <jmychalm13@gmail.com>
2023-08-03 21:33:29 -0500

    ending for the night

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ApplicationRecord
   belongs_to :user
   has_many :category_todos
-  has_many :categories, :through => :category_todos
+  has_many :categories, through: :category_todos
 end
